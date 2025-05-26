db918db06999c1799a6792f79b2ea8c41b3525bc
Matteo Solavagione <matteo.solavagione@edu-its.it>
2025-05-26 17:57:35 +0200

    add form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.querySelector('.hamburger-menu');
     const navLinks = document.querySelector('.nav-links');
 
+
+
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('active');
         navLinks.classList.toggle('active');
@@ -16,3 +18,23 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+const form = document.querySelector('form')
+
+function validateForm() {
+    const peopleNumber = document.querySelector('#people-number').value
+    const prenotationTime = document.querySelector('#prenotation-time').value
+    if(!peopleNumber.trim()){
+        alert("Selezionare il numero di persone!")
+    }
+    if(!prenotationTime.trim()){
+        alert("Selezionare l'orario!")
+    }
+    
+}
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    validateForm()
+})
