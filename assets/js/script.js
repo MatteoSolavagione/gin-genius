b6e374732406b93501cb03697889d02f9ee129a0
Matteo Solavagione <matteo.solavagione@edu-its.it>
2025-05-28 21:31:19 +0200

    aggiunti commenti al css e funzionalità js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelector('.nav-links');
 
 
-
+     //Apre il menu quando viene cliccata l'icona
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('active');
         navLinks.classList.toggle('active');
@@ -21,18 +21,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const form = document.querySelector('form')
 
+//funziona per controllare se l'utente ha selezionato un valore per ognuno dei menu a tendina
 function validateForm() {
     const peopleNumber = document.querySelector('#people-number').value
     const prenotationTime = document.querySelector('#prenotation-time').value
+    //se non ha selezionato il numero di persone manda un alert di errore
     if(!peopleNumber.trim()){
         alert("Selezionare il numero di persone!")
     }
+     //se non ha selezionato l'orario di prenotazione manda un alert di errore
     if(!prenotationTime.trim()){
         alert("Selezionare l'orario!")
     }
     
 }
-
 
 form.addEventListener('submit', function (event) {
     event.preventDefault()
